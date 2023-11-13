f3251013cd01dac488c6ccf04701870a3c172395
shantanukoyalkar12 <148475242+shantanukoyalkar12@users.noreply.github.com>
2023-11-14 01:11:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("script running ......");
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+      document.querySelector('.sidebar').classList.toggle('sidebarGo')
+})
